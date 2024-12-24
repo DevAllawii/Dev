3955ab93263805ad7dab0ba80a8a49e06bc00e75
Allawi <92133127+AllawiDev@users.noreply.github.com>
2024-12-24 23:38:55 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+const { Client } = require('discord.js-selfbot-v13');
+const client = new Client(); 
+
+client.on('ready', async () => {
+  console.log(`${client.user.username} is ready!`);
+})
+
+const { joinVoiceChannel } = require('@discordjs/voice');
+client.on('ready', () => {
+    
+    setInterval( async () => {
+    client.channels.fetch(process.env.channel) 
+     .then((channel) => { 
+      const VoiceConnection = joinVoiceChannel({
+       channelId: channel.id, 
+       guildId: process.env.guild, 
+       selfMute: true,
+       selfDeaf: true,
+       adapterCreator: channel.guild.voiceAdapterCreator 
+       });
+    }).catch((error) => { return; });
+    }, 1000)
+}); 
+
+client.login(process.env.token);
